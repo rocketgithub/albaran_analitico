221ddbb67b8384d1bde8e80924218322c3ad2d16
Rodrigo Fernandez <rodrigo@solucionesprisma.com>
2017-06-13 16:29:25 +0000

    cambios para versión 10

--- __openerp__.py
+++ __openerp__.py
@@ -1,13 +1,7 @@
 # -*- encoding: utf-8 -*-
 
-#
-# Este es el modulo de Topke
-#
-# Status 1.0 - tested on Open ERP 6.1.1
-#
-
 {
-    'name' : 'albaran_analitico',
+    'name' : 'Albaran Analitico',
     'version' : '1.0',
     'category': 'Custom',
     'description': """Agrega cuenta analitica a los movimientos contables de un albaran""",
--- stock.py
+++ stock.py
@@ -1,21 +1,18 @@
 # -*- encoding: utf-8 -*-
 
-from openerp.osv import osv, fields
-import openerp.addons.decimal_precision as dp
+from odoo import api, fields, models, _
 
-class stock_quant(osv.osv):
+class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
-    def _prepare_account_move_line(self, cr, uid, move, qty, cost, credit_account_id, debit_account_id, context=None):
-        res = super(stock_quant, self)._prepare_account_move_line(cr, uid, move, qty, cost, credit_account_id, debit_account_id, context=context)
-        if move.picking_id and move.picking_id.cuenta_analitica:
-            res[0][2]['analytic_account_id'] = move.picking_id.cuenta_analitica.id
-            res[1][2]['analytic_account_id'] = move.picking_id.cuenta_analitica.id
+    def _prepare_account_move_line(self, qty, cost, credit_account_id, debit_account_id):
+        res = super(stock_quant, self)._prepare_account_move_line(qty, cost, credit_account_id, debit_account_id)
+        if self.picking_id and self.picking_id.cuenta_analitica:
+            res[0][2]['analytic_account_id'] = self.picking_id.cuenta_analitica.id
+            res[1][2]['analytic_account_id'] = self.picking_id.cuenta_analitica.id
         return res
 
-class stock_picking(osv.osv):
+class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    _columns = {
-        'cuenta_analitica': fields.many2one('account.analytic.account', 'Cuenta analítica'),
-    }
+    cuenta_analitica = fields.Many2one('account.analytic.account', 'Cuenta analítica'),
