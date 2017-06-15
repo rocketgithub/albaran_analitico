671e51580d78db33d9e2add7ea4c9f88ab22a6d5
Rodrigo Fernandez <rodrigo@solucionesprisma.com>
2017-06-15 17:57:43 +0000

    error en nombre de campo

--- models/stock.py
+++ models/stock.py
@@ -6,10 +6,10 @@ class StockMove(models.Model):
     _inherit = 'stock.move'
 
     def _prepare_account_move_line(self, qty, cost, credit_account_id, debit_account_id):
-        res = super(stock_quant, self)._prepare_account_move_line(qty, cost, credit_account_id, debit_account_id)
-        if self.picking_id and self.picking_id.cuenta_analitica:
-            res[0][2]['analytic_account_id'] = self.picking_id.cuenta_analitica.id
-            res[1][2]['analytic_account_id'] = self.picking_id.cuenta_analitica.id
+        res = super(StockMove, self)._prepare_account_move_line(qty, cost, credit_account_id, debit_account_id)
+        if self.picking_id and self.picking_id.cuenta_analitica_id:
+            res[0][2]['analytic_account_id'] = self.picking_id.cuenta_analitica_id.id
+            res[1][2]['analytic_account_id'] = self.picking_id.cuenta_analitica_id.id
         return res
 
 class StockPicking(models.Model):
