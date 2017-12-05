a1e4dc1a9c3bb946c5a725241f1b752c9a242d64
Rodrigo Fernandez <rodrigo@solucionesprisma.com>
2017-12-05 21:51:49 +0000

    tambien obtener la cuenta analitica del context

--- models/stock.py
+++ models/stock.py
@@ -15,6 +15,13 @@ class StockMove(models.Model):
             if self.inventory_id and self.inventory_id.cuenta_analitica_id:
                 res[0][2]['analytic_account_id'] = self.inventory_id.cuenta_analitica_id.id
                 res[1][2]['analytic_account_id'] = self.inventory_id.cuenta_analitica_id.id
+
+            if self.env.context.get('analytic_account_id', False):
+                a = self.env.context.get('analytic_account_id')
+                if a.id:
+                    res[0][2]['analytic_account_id'] = a.id
+                    res[1][2]['analytic_account_id'] = a.id
+
         return res
 
 class StockPicking(models.Model):
