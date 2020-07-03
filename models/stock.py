00550f15db5594a82044522804975d2a76958838
rocketgithub <rodrigo@solucionesprisma.com>
2020-07-03 16:51:06 -0600

    Versión 13.0 :(

--- models/stock.py
+++ models/stock.py
@@ -5,8 +5,8 @@ from odoo import api, fields, models, _
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
-    def _prepare_account_move_line(self, qty, cost, credit_account_id, debit_account_id):
-        res = super(StockMove, self)._prepare_account_move_line(qty, cost, credit_account_id, debit_account_id)
+    def _prepare_account_move_line(self, qty, cost, credit_account_id, debit_account_id, description):
+        res = super(StockMove, self)._prepare_account_move_line(qty, cost, credit_account_id, debit_account_id, description)
         if len(res) > 1:
             if self.picking_id and self.picking_id.cuenta_analitica_id:
                 res[0][2]['analytic_account_id'] = self.picking_id.cuenta_analitica_id.id
