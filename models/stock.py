91c9a9ab721f06b6157b6ca8b8536fd0ce711b9f
rocketgithub <rodrigo@aquih.com>
2023-07-07 11:00:12 -0600

    Diferente firma de función y ahora se usan distribuciones de cuentas analíticas

--- models/stock.py
+++ models/stock.py
@@ -5,18 +5,18 @@ from odoo import api, fields, models, _
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
-    def _prepare_account_move_line(self, qty, cost, credit_account_id, debit_account_id, description):
-        res = super(StockMove, self)._prepare_account_move_line(qty, cost, credit_account_id, debit_account_id, description)
+    def _prepare_account_move_line(self, qty, cost, credit_account_id, debit_account_id, svl_id, description):
+        res = super(StockMove, self)._prepare_account_move_line(qty, cost, credit_account_id, debit_account_id, svl_id, description)
         if len(res) > 1:
             if self.picking_id and self.picking_id.cuenta_analitica_id:
-                res[0][2]['analytic_account_id'] = self.picking_id.cuenta_analitica_id.id
-                res[1][2]['analytic_account_id'] = self.picking_id.cuenta_analitica_id.id
+                res[0][2]['analytic_distribution'] = dict([(str(self.picking_id.cuenta_analitica_id.id), 100),])
+                res[1][2]['analytic_distribution'] = dict([(str(self.picking_id.cuenta_analitica_id.id), 100),])
 
             if self.env.context.get('analytic_account_id', False):
                 a = self.env.context.get('analytic_account_id')
                 if a.id:
-                    res[0][2]['analytic_account_id'] = a.id
-                    res[1][2]['analytic_account_id'] = a.id
+                    res[0][2]['analytic_distribution'] = dict([(str(a.id), 100),])
+                    res[1][2]['analytic_distribution'] = dict([(str(a.id), 100),])
 
         return res
 
